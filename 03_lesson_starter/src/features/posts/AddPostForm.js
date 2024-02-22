500160d9523d31cb615ad7a4953928014514aeb4
danutdotrar <morarasu.danut@gmail.com>
2024-02-22 13:53:16 +0200

    Dispatch addNewPost from postSlice

--- 03_lesson_starter/src/features/posts/AddPostForm.js
+++ 03_lesson_starter/src/features/posts/AddPostForm.js
@@ -1,39 +1,43 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
+import { quartersToYears } from "date-fns";
 
 const AddPostForm = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
-    const [userId, setUserId] = useState('')
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [userId, setUserId] = useState("");
+    const [addRequestStatus, setAddRequestStatus] = useState("idle");
 
-    const users = useSelector(selectAllUsers)
+    const users = useSelector(selectAllUsers);
 
-    const onTitleChanged = e => setTitle(e.target.value)
-    const onContentChanged = e => setContent(e.target.value)
-    const onAuthorChanged = e => setUserId(e.target.value)
+    const onTitleChanged = (e) => setTitle(e.target.value);
+    const onContentChanged = (e) => setContent(e.target.value);
+    const onAuthorChanged = (e) => setUserId(e.target.value);
+
+    const canSave =
+        [title, content, userId].every(Boolean) && addRequestStatus === "idle";
 
     const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(
-                postAdded(title, content, userId)
-            )
-            setTitle('')
-            setContent('')
+        if (canSave) {
+            try {
+                // call addNewPost from slice
+                dispatch(addNewPost({ title, body: content, userId })).unwrap();
+            } catch (error) {
+                return error.message;
+            }
         }
-    }
+    };
 
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
-
-    const usersOptions = users.map(user => (
+    const usersOptions = users.map((user) => (
         <option key={user.id} value={user.id}>
             {user.name}
         </option>
-    ))
+    ));
 
     return (
         <section>
@@ -48,7 +52,11 @@ const AddPostForm = () => {
                     onChange={onTitleChanged}
                 />
                 <label htmlFor="postAuthor">Author:</label>
-                <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+                <select
+                    id="postAuthor"
+                    value={userId}
+                    onChange={onAuthorChanged}
+                >
                     <option value=""></option>
                     {usersOptions}
                 </select>
@@ -63,9 +71,11 @@ const AddPostForm = () => {
                     type="button"
                     onClick={onSavePostClicked}
                     disabled={!canSave}
-                >Save Post</button>
+                >
+                    Save Post
+                </button>
             </form>
         </section>
-    )
-}
-export default AddPostForm
+    );
+};
+export default AddPostForm;
